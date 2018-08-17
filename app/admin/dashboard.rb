da009f79d4b00f72dc9472f7ca0b00f52fcd35f9
ventures <shivasorab@gmail.com>
2018-08-17 15:24:54 +0530

    product and category are added to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -11,23 +11,34 @@ ActiveAdmin.register_page "Dashboard" do
     end
 
     # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    
+    columns do
+      column do
+        panel "Products" do
+          ul do
+            Product.all.each do |product|
+              li link_to(product.name, admin_product_path(product))  
+              b product.category.name
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+      # column do
+      #   panel "Category" do
+      #     ul do
+      #       Category.all.each do |category|
+      #         li link_to(category.name, admin_category_path(category))
+      #       end
+      #     end
+      #   end
+      # end
+
+      # column do
+      #   panel "Info" do
+      #     para "Welcome to ActiveAdmin."
+      #   end
+      # end
+    end
+  end
 end
